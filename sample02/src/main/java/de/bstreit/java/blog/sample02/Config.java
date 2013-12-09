54a0f25b211c81700a68fb6d035419d3d3af089a
Bernhard Streit <Bernhard.Streit@gmail.com>
2013-12-10 00:41:21 +0100

    Config is a separate class now.

---
+++ sample02/src/main/java/de/bstreit/java/blog/sample02/Config.java
@@ -0,0 +1,11 @@
+package de.bstreit.java.blog.sample02;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ComponentScan
+public class Config {
+
+}
