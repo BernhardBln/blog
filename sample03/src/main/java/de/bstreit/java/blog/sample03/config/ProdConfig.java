e47ae5459207cfc0199664387cb8d216fbd62afa
Bernhard Streit <Bernhard.Streit@gmail.com>
2013-12-12 22:54:09 +0100

    Best solution so far: @Bean + @Primary

--- sample03/src/main/java/de/bstreit/java/blog/sample03/config/ProdConfig.java
+++ sample03/src/main/java/de/bstreit/java/blog/sample03/config/ProdConfig.java
@@ -1,20 +1,35 @@
 package de.bstreit.java.blog.sample03.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import javax.inject.Inject;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import de.bstreit.java.blog.sample03.StarterDev;
+import de.bstreit.java.blog.sample03.database.IDatabaseHelper;
+import de.bstreit.java.blog.sample03.database.SandboxDatabaseHelper;
 import de.bstreit.java.blog.sample03.filereader.FileReadHelper;
 import de.bstreit.java.blog.sample03.filereader.IFileReadHelper;
 
 
 @Configuration
-@Import(GeneralConfig.class)
+@ComponentScan(basePackageClasses = StarterDev.class)
 public class ProdConfig {
 
+  @Inject
+  @Primary
   @Bean
-  public IFileReadHelper getFileReadHelper() {
-    return new FileReadHelper();
+  public IDatabaseHelper getDatabaseHelper(SandboxDatabaseHelper databaseHelper) {
+    return databaseHelper;
+  }
+
+  @Inject
+  @Primary
+  @Bean
+  public IFileReadHelper getFileReadHelper(FileReadHelper fileReadHelper) {
+    return fileReadHelper;
   }
 
 }
--- sample03/src/test/java/de/bstreit/java/blog/sample03/config/TestConfig.java
+++ sample03/src/test/java/de/bstreit/java/blog/sample03/config/TestConfig.java
@@ -1,27 +1,35 @@
 package de.bstreit.java.blog.sample03.config;
 
+import javax.inject.Inject;
+
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
 
 import de.bstreit.java.blog.sample03.JUnitDatabaseMock;
+import de.bstreit.java.blog.sample03.StarterDev;
 import de.bstreit.java.blog.sample03.database.IDatabaseHelper;
 import de.bstreit.java.blog.sample03.filereader.IFileReadHelper;
 import de.bstreit.java.blog.sample03.filereader.SandboxFileReadHelper;
 
 
 @Configuration
-@Import(GeneralConfig.class)
+@ComponentScan(basePackageClasses = StarterDev.class)
 public class TestConfig {
 
+  @Inject
+  @Primary
   @Bean
-  public IDatabaseHelper getDatabaseHelper() {
-    return new JUnitDatabaseMock();
+  public IDatabaseHelper getDatabaseHelper(JUnitDatabaseMock databaseHelper) {
+    return databaseHelper;
   }
 
+  @Inject
+  @Primary
   @Bean
-  public IFileReadHelper getFileReadHelper() {
-    return new SandboxFileReadHelper();
+  public IFileReadHelper getFileReadHelper(SandboxFileReadHelper fileReadHelper) {
+    return fileReadHelper;
   }
 
 }
